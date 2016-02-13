72cdc2245fb761919bc59f610ffe12b4ffefaae2
Glauber Gallego <glauber.gallego@gmail.com>
2016-02-13 21:46:10 -0200

    better format for exclusion list

--- src/main/scala/aws/daleks/eager/EagerS3Dalek.scala
+++ src/main/scala/aws/daleks/eager/EagerS3Dalek.scala
@@ -22,7 +22,10 @@ class EagerS3Dalek(implicit region: Region, credentials: AWSCredentialsProvider)
 
   def buckets = (s3.listBuckets.asScala).filter { bucket =>
     val name = bucket.getName
-    val keep =  name.startsWith("logs")  || name.startsWith("billing") || name.startsWith("share") || name.endsWith("do-not-delete")
+    val keep =  name.startsWith("logs") || 
+                name.startsWith("billing") || 
+                name.startsWith("share") || 
+                name.endsWith("do-not-delete")
     !keep
   }.filter { bucket =>
     val locStr = s3.getBucketLocation(bucket.getName)
